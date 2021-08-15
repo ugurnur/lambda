2abbb3320d9552e65c2bbac51bdc9456faf73611
ofazli <fazliorcun@gmail.com>
2021-08-15 14:50:02 -0400

    Lambda

--- src/lambdacourse/FuncitonalPrgrammin02.java
+++ src/lambdacourse/FuncitonalPrgrammin02.java
@@ -53,14 +53,19 @@ public class FuncitonalPrgrammin02 {
 
 
     public static void printElFunctional(List<Integer> l) {
-        l.stream().forEach(Utils::printInTheSameLineWithASpace); //12 9 13 4 9 2 4 12 15
+                l.
+                stream().
+                forEach(Utils::printInTheSameLineWithASpace); //12 9 13 4 9 2 4 12 15
     }
 
     //2)Create a method to print the even list elements on the console in the same line with a space between two consecutive
     // elements.(Functional)
 
     public static void PrintEvensFunctional(List<Integer> l) {
-        l.stream().filter(Utils ::checkToBeEven).forEach(Utils::printInTheSameLineWithASpace); //12 4 2 4 12
+                 l.
+                stream().
+                filter(Utils ::checkToBeEven).
+                forEach(Utils::printInTheSameLineWithASpace); //12 4 2 4 12
     }
 
     //3)Create a method to print the square of odd list elements on the console in the same line with a space
@@ -68,21 +73,35 @@ public class FuncitonalPrgrammin02 {
 
     public static void printSquaresOfOdd (List<Integer> l) {
 
-        l.stream().filter(Utils ::checkToBeOdd).map(Utils::getSqure).forEach(Utils::printInTheSameLineWithASpace); //81 169 81 225
+                 l.
+                stream().
+                filter(Utils ::checkToBeOdd).
+                map(Utils::getSqure).
+                forEach(Utils::printInTheSameLineWithASpace); //81 169 81 225
 
     }
     //4)Create a method to print the cube of distinct odd list elements on the console in the same line with a
     //space between two consecutive elements.
 
     public static void printCubeOfDistinctOdds (List<Integer> l) {
-        l.stream().distinct().filter(Utils::checkToBeOdd).map(Utils::getCube).forEach(Utils::printInTheSameLineWithASpace); //729 2197 3375
+                  l.
+                stream().
+                distinct().
+                filter(Utils::checkToBeOdd).
+                map(Utils::getCube).
+                forEach(Utils::printInTheSameLineWithASpace); //729 2197 3375
     }
 
     //5)Create a method to calculate the sum of the squares of distinct even elements
 
     public static void sumOfSquareDistinctEvens (List<Integer> l) {
 
-        Integer sum = l.stream().distinct().filter(Utils::checkToBeEven).map(Utils::getSqure).reduce(0, Math::addExact);
+        Integer sum = l.
+                        stream().
+                        distinct().
+                        filter(Utils::checkToBeEven).
+                        map(Utils::getSqure).
+                        reduce(0, Math::addExact);
 
         System.out.println("The sum of the sqaures of even distinct elements is " + sum);
         //The sum of the sqaures of even distinct elements is 164
@@ -91,7 +110,12 @@ public class FuncitonalPrgrammin02 {
     //6)Create a method to calculate the product of the cubes of distinct even elements
     public static void productOfCubeSutinctEvens (List<Integer> l) {
 
-        Integer prod = l.stream().distinct().filter(Utils::checkToBeEven).map(Utils::getCube).reduce(1, Math::multiplyExact);
+        Integer prod = l.
+                        stream().
+                        distinct().
+                        filter(Utils::checkToBeEven).
+                        map(Utils::getCube).
+                        reduce(1, Math::multiplyExact);
 
         System.out.println("The multiplication of the cubes of even distinct elements is " + prod);
         //The multiplication of the cubes of even distinct elements is 884736
@@ -100,7 +124,9 @@ public class FuncitonalPrgrammin02 {
     //7)Create a method to find the maximum value from the list elements
     public static void getMaxElement (List<Integer> l) {
 
-        Integer maxEL = l.stream().reduce(Integer.MIN_VALUE, Math :: max);
+        Integer maxEL = l.
+                        stream().
+                        reduce(Integer.MIN_VALUE, Math :: max);
 
         System.out.println("The max element is  " + maxEL);
         //The max element is  15
@@ -110,7 +136,9 @@ public class FuncitonalPrgrammin02 {
 
     public static void getMinElements (List<Integer> l) {
 
-        Integer minEL = l.stream().reduce(Integer.MAX_VALUE, Math ::min);
+        Integer minEL = l.
+                        stream().
+                        reduce(Integer.MAX_VALUE, Math ::min);
         System.out.println("The min element is  " + minEL);
         //The min element is  2
     }
@@ -119,7 +147,12 @@ public class FuncitonalPrgrammin02 {
 
     public static void getMinGreaterThanSevenEven(List<Integer> l) {
 
-        Integer minEl=l.stream().distinct().filter(t->t>7).filter(Utils::checkToBeEven).reduce(Integer.MAX_VALUE, Math::min);
+        Integer minEl=l.
+                        stream().
+                        distinct().
+                        filter(t->t>7).
+                        filter(Utils::checkToBeEven).
+                        reduce(Integer.MAX_VALUE, Math::min);
 
         System.out.println("1)The min element is  " + minEl);
         //1)The min element is  12
@@ -128,7 +161,12 @@ public class FuncitonalPrgrammin02 {
     //11)Create a method to find the half of the elements which are distinct and greater than 5 in reverse in the list.
     public static void getHalfOfDistinctElReversed (List<Integer> l) {
 
-        List<Double> resultList =	l.stream().distinct().filter(t->t>5).map(Utils::getHalf).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        List<Double> resultList =	l.
+                                    stream().distinct().
+                                    filter(t->t>5).
+                                    map(Utils::getHalf).
+                                    sorted(Comparator.reverseOrder()).
+                                    collect(Collectors.toList());
 
         System.out.println("Half of the elements list :  " + resultList);
         //Half of the elements list :  [7.5, 6.5, 6.0, 4.5]
