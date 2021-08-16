fbfffcf5dfc4c343e8f1e5ad5ac4c3eb4fdea2ba
ofazli <fazliorcun@gmail.com>
2021-08-16 18:27:45 -0400

    Lambda

--- src/lambdacourse/DenemeLambda.java
+++
@@ -1,9 +0,0 @@
-package lambdacourse;
-
-public class DenemeLambda {
-    public static void main(String[] args) {
-        System.out.println("deneme icindir");
-        System.out.println("deneme icindir");
-        System.out.println("Bir deneme daha ");
-    }
-}
