954784d331163797cf0a696e37fb5478ef5716b3
ofazli <fazliorcun@gmail.com>
2021-08-15 22:44:41 -0400

    Lambda

--- src/lambdacourse/DenemeLambda.java
+++ src/lambdacourse/DenemeLambda.java
@@ -3,5 +3,6 @@ package lambdacourse;
 public class DenemeLambda {
     public static void main(String[] args) {
         System.out.println("deneme icindir");
+        System.out.println("deneme icindir");
     }
 }
